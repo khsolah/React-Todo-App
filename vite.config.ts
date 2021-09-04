6e4bc82aabacb0f7c9c51860e03a766a2b0d0e4e
khsolah <khsolah@gmail.com>
2021-09-04 22:13:56 +0800

    chore: set resolve path alias and resolve vite warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import alias from '@rollup/plugin-alias'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    alias({
+      entries: [{ find: '~', replacement: '/src' }]
+    }),
+    reactRefresh()
+  ],
+  server: {
+    fs: {
+      allow: ['..'],
+      strict: false
+    }
+  }
 })
