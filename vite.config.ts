11d56a707361ad24ae14add79c36b0f96db0bdb4
khsolah <khsolah@gmail.com>
2021-09-05 21:34:31 +0800

    chore: setup windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import alias from '@rollup/plugin-alias'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import windicss from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,8 @@ export default defineConfig({
     alias({
       entries: [{ find: '~', replacement: '/src' }]
     }),
-    reactRefresh()
+    reactRefresh(),
+    windicss()
   ],
   server: {
     fs: {
